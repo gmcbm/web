4528d6da6f5b437f8639b35c7257ea2ebd3b4564
Ursin Filli <git@ursinn.dev>
2021-04-03 19:36:37 +0200

    Design Test

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,14 @@
-const Footer = () => {
+export default function Footer() {
     return (
         <footer className="bg-gray-800">
             <div className="h-16 flex items-center justify-center">
                 <span className="text-center text-gray-300">Copyright &copy; {new Date().getFullYear()} <a href="https://gmcbm.net">GMCBM.net</a></span>
             </div>
+            <div className="h-12 flex justify-center space-x-16">
+                <span className="text-center text-gray-300"><a href="https://gmcbm.net">Imprint</a></span>
+                <span className="text-center text-gray-300"><a href="https://gmcbm.net">Privacy Police </a></span>
+                <span className="text-center text-gray-300"><a href="https://gmcbm.net">Terms of Service</a></span>
+            </div>
         </footer>
     )
 }
-
-export default Footer
