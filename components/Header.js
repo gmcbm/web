b21b65e4eb384af6f0f6364b6c006edc902153b6
Ursin Filli <git@ursinn.dev>
2021-04-03 21:43:43 +0200

    Design Test

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,9 @@ export default function Header() {
         <Head>
             <title>GMCBM</title>
             <meta property="og:title" content="GMCBM" key="title"/>
+            <link rel="alternate" hrefLang="en" href="https://gmcbm.net/en"/>
+            <link rel="alternate" hrefLang="de" href="https://gmcbm.net/de"/>
+            <link rel="alternate" hrefLang="x-default" href="https://gmcbm.net/"/>
         </Head>
     )
 }
