e19f2d2df9d67aa3a8613f2927e2a0e95779b3af
Ursin Filli <git@ursinn.dev>
2021-04-03 20:11:31 +0200

    Design Test

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,4 +1,9 @@
+import { useState } from 'react'
+import { Transition } from '@headlessui/react'
+
 export default function Navigation() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <nav className="bg-gray-800">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -10,6 +15,7 @@ export default function Navigation() {
                         </div>
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-baseline space-x-4">
+                                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                                 <a href="#"
                                    className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
 
@@ -33,6 +39,7 @@ export default function Navigation() {
                             <button
                                 className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                 <span className="sr-only">View notifications</span>
+                                {/* Heroicon name: outline/bell */}
                                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
                                      viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -40,9 +47,10 @@ export default function Navigation() {
                                 </svg>
                             </button>
 
+                            {/* Profile dropdown */}
                             <div className="ml-3 relative">
                                 <div>
-                                    <button type="button"
+                                    <button type="button" onClick={() => setIsOpen(!isOpen)}
                                             className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                             id="user-menu" aria-expanded="false" aria-haspopup="true">
                                         <span className="sr-only">Open user menu</span>
@@ -51,32 +59,54 @@ export default function Navigation() {
                                              alt="" />
                                     </button>
                                 </div>
-                                <div
-                                    className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                    role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                       role="menuitem">Your Profile</a>
+                                <Transition
+                                    show={isOpen}
+                                    enter="transition ease-out duration-100"
+                                    enterFrom="transform opacity-0 scale-95"
+                                    enterTo="transform opacity-100 scale-100"
+                                    leave="transition ease-in duration-75"
+                                    leaveFrom="transform opacity-100 scale-100"
+                                    leaveTo="transform opacity-0 scale-95"
+                                >
+                                    {(ref) => (
+                                        <div ref={ref}  className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                               role="menuitem">Your Profile</a>
 
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                       role="menuitem">Settings</a>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                               role="menuitem">Settings</a>
 
-                                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                       role="menuitem">Sign out</a>
-                                </div>
+                                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                               role="menuitem">Sign out</a>
+                                        </div>
+                                    )}
+                                </Transition>
                             </div>
                         </div>
                     </div>
 
                     <div className="-mr-2 flex md:hidden">
+                        {/* Mobile menu button */}
                         <button type="button"
                                 className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                 aria-controls="mobile-menu" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
+                            {/*
+                            Heroicon name: outline/menu
+
+                            Menu open: "hidden", Menu closed: "block"
+                            */}
                             <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                       d="M4 6h16M4 12h16M4 18h16"/>
                             </svg>
+                            {/*
+                            Heroicon name: outline/x
+
+                            Menu open: "block", Menu closed: "hidden"
+                            */}
                             <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -87,8 +117,10 @@ export default function Navigation() {
                 </div>
             </div>
 
+            {/* Mobile menu, show/hide based on menu state */}
             <div className="md:hidden" id="mobile-menu">
                 <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                     <a href="#"
                        className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Dashboard</a>
 
@@ -118,6 +150,7 @@ export default function Navigation() {
                         <button
                             className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                             <span className="sr-only">View notifications</span>
+                            {/* Heroicon name: outline/bell */}
                             <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
